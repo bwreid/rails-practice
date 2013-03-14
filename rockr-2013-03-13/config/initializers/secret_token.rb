63730076fbae0b653117d86a84f562cdb3beeda0
Bryan W. Reid <bwreid@gmail.com>
2013-03-14 10:15:14 -0400

    respawn point

---
+++ rockr-2013-03-13/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rockr20130313::Application.config.secret_token = '402205a422fb918ae96257363ce6bd51b927eac0f25394199c87f4ece06eb5a6d9d6181931396275cbbf965408f107689e3695892bb2e172cd40f2827d8b8823'
